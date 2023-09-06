5f351399957490c300388fb61129433657792276
Gong Zhang <zhanggong@me.com>
2023-09-06 18:46:56 +0800

    Update build-xcframework

--- build-xcframework.sh
+++ build-xcframework.sh
@@ -19,4 +19,10 @@ case $answer in
 esac
 
 ./tools/build-cocoa.sh -b
+
+# rename to standard name
+# realm-monorepo-xcframework-v12.4.0-7-g0b90cacec.tar.xz => realm-core-12.4.0-xcframework.tar.xz
+SEMVER=$(git describe | cut -d'-' -f1 | cut -c2-)
+mv "realm-monorepo-xcframework-$VERSION.tar.xz" "realm-core-$SEMVER-xcframework.tar.xz"
+
 open .
