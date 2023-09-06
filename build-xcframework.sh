2a496d2e50b87af7e61f6bf28ff015db29ec8dd5
Gong Zhang <zhanggong@me.com>
2023-09-06 16:32:07 +0800

    Add build-xcframework.sh

---
+++ build-xcframework.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+set -e
+
+VERSION=$(git describe)
+
+echo "Build XCframework (librealm-monorepo)"
+echo "DEVELOPER_DIR=$DEVELOPER_DIR"
+echo "VERSION=$VERSION"
+
+echo "Do you want to continue? (y/n)"
+read -r answer
+case $answer in
+    [yY][eE][sS]|[yY])
+        echo "building..."
+        ;;
+    *)
+        exit 0
+        ;;
+esac
+
+./tools/build-cocoa.sh -b
